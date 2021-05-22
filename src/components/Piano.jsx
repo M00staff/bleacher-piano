c127491ad6f7f7c76a2a4e037cc5e1dbf80fab84
Mostafa Habib <mhabib@novetta.com>
2021-05-22 03:16:32 -0400

    refs and timeouts working, borderline mvp

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,20 +1,28 @@
 import React, { Fragment, useState, useRef } from "react"
 
 export const Piano = () => {
+  const pianoKeys = ["C", "D", "E", "F", "G", "A", "B"]
   const [keys, setKeys] = useState([])
-  const keyRef = useRef()
+  const keyRef = useRef([])
+
   const addKeys = (letter) => {
     setKeys([...keys, letter]);
   };
-  const pianoKeys = ["C", "D", "E", "F", "G", "A", "B"]
+
+  const fireKeys = () => {
+    keys.map((x, i) => (
+      setTimeout(() => (
+        keyRef.current[x].focus()
+      ), 1000 * i)))
+  }
 
   return (
     <Fragment>
       <div className="keyWrapper">
-        {pianoKeys.map(x => <button ref={keyRef} onClick={() => addKeys(x)}>{x}</button>)}
+        {pianoKeys.map(x => <button key={x} ref={(e) => keyRef.current[x] = e} onClick={() => addKeys(x)}>{x}</button>)}
       </div>
       <div className="controlsWrapper">
-        <div className="notes" onClick={() => keyRef.current.focus()}>Play</div>
+        <div className="notes" onClick={() => fireKeys()}>Play</div>
         <div className="notes">{keys}</div>
         <div className="notes" onClick={() => setKeys([])}>Clear</div>
       </div>
