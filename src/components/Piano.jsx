7f3b49f5f4836febb26d0a2f5ece865996c317d5
Mostafa Habib <mhabib@novetta.com>
2021-05-24 01:02:11 -0400

    add textbox functionality, and simple keyframe, refactor - at MVP

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -3,28 +3,63 @@ import React, { Fragment, useState, useRef } from "react"
 export const Piano = () => {
   const pianoKeys = ["C", "D", "E", "F", "G", "A", "B"]
   const [keys, setKeys] = useState([])
+  const [inputKeys, setInputKeys] = useState("")
   const keyRef = useRef([])
 
-  const addKeys = (letter) => {
+  const addKeys = (letter, event) => {
+    event.target.classList.add("clickedButton")
     setKeys([...keys, letter]);
   };
 
-  const fireKeys = () => {
-    keys.map((x, i) => (
-      setTimeout(() => (
-        keyRef.current[x].focus()
-      ), 1000 * i)))
+  const clearKeys = () => {
+    setKeys([])
+    setInputKeys("")
+  }
+
+  const validateInput = () => {
+    [...inputKeys].forEach((x, i) => {
+      if (i % 2 === 0) {
+        if (pianoKeys.includes(x)) {
+          setTimeout(() => (
+            keyRef.current[x].click()
+          ), 1000 * i)
+        } else {
+          console.error("ERROR - key must exist in piano keys");
+        }
+      }
+      else {
+        if (x !== ",") {
+          console.error("ERROR - needs to be comma delimited");
+        }
+      }
+    });
   }
 
   return (
     <Fragment>
       <div className="keyWrapper">
-        {pianoKeys.map(x => <button key={x} ref={(e) => keyRef.current[x] = e} onClick={() => addKeys(x)}>{x}</button>)}
+        {pianoKeys.map(x =>
+          <button
+            className="keyButton"
+            key={x}
+            ref={(e) => keyRef.current[x] = e}
+            onClick={(e) => addKeys(x, e)}
+            onAnimationEnd={(e) => e.target.classList.remove("clickedButton")}
+          >
+            {x}
+          </button>)}
       </div>
       <div className="controlsWrapper">
-        <div className="notes" onClick={() => fireKeys()}>Play</div>
-        <div className="notes">{keys}</div>
-        <div className="notes" onClick={() => setKeys([])}>Clear</div>
+        <div className="notes">
+          {keys.length ? keys.join() : null}
+        </div>
+        <div className="notes" onClick={() => clearKeys([])}>Clear</div>
+      </div>
+      <div className="controlsWrapper">
+        <form>
+          <input type="text" name="keys" value={inputKeys} onChange={(e) => setInputKeys(e.target.value)} />
+          <input type="button" value="Play" onClick={() => validateInput()} />
+        </form>
       </div>
     </Fragment>
   )
